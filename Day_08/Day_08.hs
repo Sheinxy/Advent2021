23740375ad1039cc0789c4d7a6b804733067887d
raphael.montes <raphael.montes@epita.fr>
2021-12-09 02:23:10 +0100

    Removed useless operators

--- Day_08/Day_08.hs
+++ Day_08/Day_08.hs
@@ -26,12 +26,12 @@ countUniques = length . filter isUnique . concat . map snd
 findMapping :: [String] -> Map.Map String Int
 findMapping [one, seven, four, x1, x2, x3, x4, x5, x6, eight] = Map.fromList $ zip [zero, one, two, three, four, five, six, seven, eight, nine] [0 .. ]
   where findWith f = head . filter f 
-        two = findWith ((==) 7 . length . union four) $ [x1, x2, x3]
-        three = findWith ((==) 5 . length . union one) $ [x1, x2, x3]
-        five = findWith (\x -> x /= three && x /= two) $ [x1, x2, x3]
-        six = findWith ((==) 7 . length . union one)  $ [x4, x5, x6]
-        nine = findWith ((==) 6 . length . union four) $ [x4, x5, x6]
-        zero = findWith (\x -> x /= six && x /= nine) $ [x4, x5, x6]
+        two = findWith ((==) 7 . length . union four) [x1, x2, x3]
+        three = findWith ((==) 5 . length . union one) [x1, x2, x3]
+        five = findWith (\x -> x /= three && x /= two) [x1, x2, x3]
+        six = findWith ((==) 7 . length . union one)  [x4, x5, x6]
+        nine = findWith ((==) 6 . length . union four) [x4, x5, x6]
+        zero = findWith (\x -> x /= six && x /= nine) [x4, x5, x6]
 
 outputValue :: Entry -> Int
 outputValue (input, output) = foldl (\x y -> 10 * x + dm Map.! y) 0 $ map sort output
