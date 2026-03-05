6a9420ed0d70e88fb7c20c23f36007d6192e2caa
Sheinxy <sheinxy@seascow.win>
2026-03-06 00:47:47 +0100

    Cleanup

--- Day_08/Day_08.hs
+++ Day_08/Day_08.hs
@@ -24,17 +24,18 @@ countUniques :: [Entry] -> Int
 countUniques = length . filter isUnique . concat . map snd
 
 findMapping :: [String] -> Map.Map String Int
-findMapping [one, seven, four, x1, x2, x3, x4, x5, x6, eight] = Map.fromList [(one, 1), (two, 2), (three, 3), (four, 4), (five, 5), (six, 6), (seven, 7), (eight, 8), (nine, 9), (zero, 0)]
-  where two = head . filter((==) 7 . length . union four) $ [x1, x2, x3]
-        three = head . filter ((==) 5 . length . union one) $ [x1, x2, x3]
-        five = head . filter (\x -> x /= three && x /= two) $ [x1, x2, x3]
-        six = head . filter ((==) 7 . length . union one)  $ [x4, x5, x6]
-        nine = head . filter ((==) 6 . length . union (union five one)) $ [x4, x5, x6]
-        zero = head . filter (\x -> x /= six && x /= nine) $ [x4, x5, x6]
+findMapping [one, seven, four, x1, x2, x3, x4, x5, x6, eight] = Map.fromList $ zip [zero, one, two, three, four, five, six, seven, eight, nine] [0 .. ]
+  where findWith f = head . filter f 
+        two = findWith ((==) 7 . length . union four) $ [x1, x2, x3]
+        three = findWith ((==) 5 . length . union one) $ [x1, x2, x3]
+        five = findWith (\x -> x /= three && x /= two) $ [x1, x2, x3]
+        six = findWith ((==) 7 . length . union one)  $ [x4, x5, x6]
+        nine = findWith ((==) 6 . length . union (union five one)) $ [x4, x5, x6]
+        zero = findWith (\x -> x /= six && x /= nine) $ [x4, x5, x6]
 
 outputValue :: Entry -> Int
 outputValue (input, output) = foldl (\x y -> 10 * x + dm Map.! y) 0 $ map sort output
-  where dm = findMapping $ sortBy (comparing length) $ map sort input
+  where dm = findMapping . sortBy (comparing length) $ map sort input
 
 sumAllOutput :: [Entry] -> Int
 sumAllOutput = sum . map outputValue
