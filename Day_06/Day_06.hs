3a02d5bb447c96067d6b1065df26ee0939f17e5e
Sheinxy <sheinxy@seascow.win>
2026-03-06 00:47:47 +0100

    Hard coding things > all

--- Day_06/Day_06.hs
+++ Day_06/Day_06.hs
@@ -7,8 +7,8 @@ parseInput input = map (\x -> length . filter (== x) $ fishes) [0 .. 8]
     where fishes = map read . splitOn "," $ input
 
 newDay :: [Int] -> [Int]
-newDay school = map (\(x, y) -> y + if x == 6 then head school else 0) shift 
-  where shift = zip [0 .. ] $ tail school ++ [head school]
+newDay [x0, x1, x2, x3, x4, x5, x6, x7, x8] = [x1, x2, x3, x4, x5, x6, x7 + x0, x8, x0]
+newDay _ = error "Invalid school"
 
 nDays :: Int -> [Int] -> Int
 nDays n = sum . flip (!!) n . iterate newDay
