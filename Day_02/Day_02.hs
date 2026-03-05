30e4446794e2ab9c0d9d75e3f498df3388471268
Sheinxy <sheinxy@seascow.win>
2026-03-06 00:47:46 +0100

    Simplified solution

--- Day_02/Day_02.hs
+++ Day_02/Day_02.hs
@@ -1,20 +1,12 @@
 module Main where
 
-move :: (Int, Int) -> (String, Int) -> (Int, Int)
-move (height, depth) ("forward", amt) = (height + amt, depth)
-move (height, depth) ("down", amt) = (height, depth + amt)
-move (height, depth) (_, amt) = (height, depth - amt)
+move :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
+move (height, depth, aim) ("forward", amt) = (height + amt, depth + aim * amt, aim)
+move (height, depth, aim) ("down", amt) = (height, depth, aim + amt)
+move (height, depth, aim) (_, amt) = (height, depth, aim - amt)
 
-moveAround :: [(String, Int)] -> (Int, Int)
-moveAround = foldl move (0, 0)
-
-moveAndAim :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
-moveAndAim (height, depth, aim) ("forward", amt) = (height + amt, depth + aim * amt, aim)
-moveAndAim (height, depth, aim) ("down", amt) = (height, depth, aim + amt)
-moveAndAim (height, depth, aim) (_, amt) = (height, depth, aim - amt)
-
-moveAroundAndAim :: [(String, Int)] -> (Int, Int, Int)
-moveAroundAndAim = foldl moveAndAim (0, 0, 0)
+moveAround :: [(String, Int)] -> (Int, Int, Int)
+moveAround = foldl move (0, 0, 0)
 
 readInt :: String -> Int
 readInt = read
@@ -26,7 +18,6 @@ parseLine l = (dir, readInt amt)
 main = do
   content <- readFile "input"
   let input = map (parseLine . words) . lines $ content
-  let (h, d) = moveAround input
-  print (h * d)
-  let (h, d, a) = moveAroundAndAim input
+  let (h, d, a) = moveAround input
+  print (h * a)
   print (h * d)
